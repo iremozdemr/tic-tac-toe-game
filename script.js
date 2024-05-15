5fb2f04e51c1b1f4f88d6626d32ba0d6d946031c
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-05-15 19:00:35 +0300

    adding js codes

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
-const actionsButton = document.getElementsByClassName("actions-button")[0];
-const resetButton = document.getElementsByClassName("reset-button")[0];
-const newRoundButton = document.getElementsByClassName("new-round-button")[0];
+const actionsButton = document.querySelector(".actions-button");
+const resetButton = document.querySelector("reset-button");
+const newRoundButton = document.querySelector("new-round-button");
+const items = document.querySelectorAll(".items");
 let showed = false;
+
+const squares = document.querySelectorAll(".square");
+const p1Wins = document.querySelector("#p1-wins");
+const ties = document.querySelector("#ties");
+const p2Wins = document.querySelector("#p2-wins");
+const turnDiv = document.querySelector(".turn");
+
+let currentPlayer = 1;
+let p1Count = 0;
+let tiesCount = 0;
+let p2Count = 0;
+let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function toggleMenu(){
     if(showed == false){
-        resetButton.style.display = "block";
-        newRoundButton.style.display = "block";
+        items[0].style.display = "block";
+        items[1].style.display = "block";
         showed = true;
     }
     else if(showed == true){
-        resetButton.style.display = "none";
-        newRoundButton.style.display = "none";
+        items[0].style.display = "none";
+        items[1].style.display = "none";
         showed = false;
     }
 }
 
 actionsButton.addEventListener("click", toggleMenu);
+
+function clickSquare(event){
+    const index = parseInt(event.target.id)-1;
+    if(currentPlayer == 1 && board[index] == 0){
+        board[index] = 1;
+        event.target.textContent = "x";
+        currentPlayer = 2;
+    }
+    else if(currentPlayer == 2 && board[index] == 0){
+        board[index] = 2;
+        event.target.textContent = "o";
+        currentPlayer = 1;
+    }
+    updateTurn();
+}
+
+function updateTurn(){
+    if(currentPlayer == 1){
+        turnDiv.textContent = "player 1, you're up";
+    }
+    else{
+        turnDiv.textContent = "player 2, you're up";
+    }
+}
+
+for(let i=0; i<9; i++){
+    squares[i].addEventListener("click",clickSquare);
+}
